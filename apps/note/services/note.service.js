4d6d8d02008075631d5631a18b04f4a4d30751ab
Noahmarkovich <noahmarko93@gmail.com>
2022-12-28 17:33:48 +0200

    added video note

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,5 +1,5 @@
 
-import {utilService} from "../../../services/util.service.js"
+import { utilService } from "../../../services/util.service.js"
 
 export const noteService = {
     getNotes
@@ -9,48 +9,55 @@ export const noteService = {
 const NOTE_KEY = "noteDB"
 // _createNotes()
 
-function getNotes(){
+function getNotes() {
     return _createNotes()
 }
 
 
-function _createNotes(){    
+function _createNotes() {
     let notes = utilService.loadFromStorage(NOTE_KEY)
-    if (!notes || !notes.length){
+    if (!notes || !notes.length) {
         notes = [
-        {
-         id: utilService.makeId(),
-         type: "note-txt",
-         isPinned: true,
-         info: {
-         txt: "Fullstack Me Baby!"
-         }
-        },
-        {
-         id: utilService.makeId(),
-         type: "note-img",
-         info: {
-         url: "/assets/img/puppy.jpg",
-         title: "Feed the dog"
-         },
-         style: {
-         backgroundColor: "#00d"
-         }
-        },
-        {
-         id: utilService.makeId(),
-         type: "note-todos",
-         info: {
-         label: "Get my stuff together",
-         todos: [
-         { txt: "Driving liscence", doneAt: null },
-         { txt: "Coding power", doneAt: 187111111 }
-         ]
-         }
-        }
+            {
+                id: utilService.makeId(),
+                type: "note-txt",
+                isPinned: true,
+                info: {
+                    txt: "Fullstack Me Baby!"
+                }
+            },
+            {
+                id: utilService.makeId(),
+                type: "note-img",
+                info: {
+                    url: "/assets/img/puppy.jpg",
+                    title: "Feed the dog"
+                },
+                style: {
+                    backgroundColor: "#00d"
+                }
+            },
+            {
+                id: utilService.makeId(),
+                type: "note-todos",
+                info: {
+                    label: "Get my stuff together",
+                    todos: [
+                        { txt: "Driving liscence", doneAt: null },
+                        { txt: "Coding power", doneAt: 187111111 }
+                    ]
+                }
+            },
+            {
+                id: utilService.makeId(),
+                type: "note-video",
+                info: {
+                    url: "https://www.youtube.com/watch?v=GTCd0hmjHBs&t=1s",
+                }
+            }
         ]
         utilService.saveToStorage(NOTE_KEY, notes)
     }
     return notes
-        
+
 }
