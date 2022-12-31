fd1670b22bcd15827bd2653f3d8299deac226ed0
Alon Mlievski <alonmlievski@gmail.com>
2023-01-01 00:03:42 +0200

    about page design

--- views/about.jsx
+++ views/about.jsx
@@ -1,21 +1,22 @@
-
 export function About() {
-    return <section className="about">
-<div className="alon">
-    <img src="" alt="" />
-    <h1>Alon Mlievski</h1>
-    <h2>Full Stack Developer</h2>
-    <h4>Coding Academy student</h4>
-    <h4>Based in Tel Aviv</h4>
-</div>
-<div className="noah">
-    <img src="" alt="" />
-    <h1>Noah Markovich</h1>
-    <h2>Full Stack Developer</h2>
-    <h4>Coding Academy student</h4>
-    <h4>Based in Tel Aviv</h4>
-</div>
+  return (
+    <section className="about">
+      <div className="authors">
+        <div className="alon">
+          <img src="../assets/img/authots/Alon.jpg" alt="" />
+          <h1 className="about-name">Alon Mlievski</h1>
+          <h2 className="about-title">Full Stack Developer</h2>
+          <h4 className="about-desc">Coding Academy student</h4>
+          <h4 className="about-desc">Based in Tel Aviv</h4>
+        </div>
+        <div className="noah">
+        <img src="../assets/img/authots/Noah.jpeg" alt="" />
+          <h1 className="about-name">Noah Markovich</h1>
+          <h2 className="about-title">Full Stack Developer</h2>
+          <h4 className="about-desc">Coding Academy student</h4>
+          <h4 className="about-desc">Based in Tel Aviv</h4>
+        </div>
+      </div>
     </section>
+  );
 }
-
-
