a1e4541080359ea8a33f52d76a5322aa281e93eb
Alon Mlievski <alonmlievski@gmail.com>
2023-01-01 01:20:56 +0200

    debugging

--- views/home.jsx
+++ views/home.jsx
@@ -10,14 +10,14 @@ export function Home() {
     }
          return <section className="home">
         {/* <h1>Appsus</h1> */}
-        <h2>Appsus- Your Personal Workspace</h2>
+        <h2>Appsus - Your Personal Workspace</h2>
         <div className="apps">
         <div onClick={(ev)=>moveTo(ev, 'note')} className="keep">
-        <img src="/assets/img/post-it.png" alt="" />
+        <img src="ץ/assets/img/post-it.png" alt="" />
         <h1>Keep your thoughts clear</h1>
         </div>
         <div onClick={(ev)=>moveTo(ev, 'mail')} className="mail">
-        <img src="/assets/img/inbox.png" alt="" />
+        <img src="./assets/img/inbox.png" alt="" />
         <h1>Write a friend</h1>
         </div>
         </div>
