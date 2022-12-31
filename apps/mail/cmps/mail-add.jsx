3acc27a9dc280b74540d3d9a1744f40e636be5ca
Alon Mlievski <alonmlievski@gmail.com>
2022-12-31 02:30:37 +0200

    initial email form, homepage and about pages design

--- apps/mail/cmps/mail-add.jsx
+++ apps/mail/cmps/mail-add.jsx
@@ -7,48 +7,55 @@ import {
   showSuccessMsg,
 } from "../services/event-bus.service.js";
 
-export function MailAdd({onSendMail}) {
-  const [mailToAdd, setMailToAdd] = useState(mailService.getTemplateMail())
+export function MailAdd({ onSendMail }) {
+  const [mailToAdd, setMailToAdd] = useState(mailService.getTemplateMail());
 
   function handleChange({ target }) {
-      let { value, name: field } = target
-      setMailToAdd((prevMail) => ({ ...prevMail, [field]: value }))
+    let { value, name: field } = target;
+    setMailToAdd((prevMail) => ({ ...prevMail, [field]: value }));
   }
 
   return (
     <section className="mail-add">
+      <div className="mail-add-header">New Message</div>
       <form onSubmit={() => onSendMail(event, mailToAdd)}>
-        <label htmlFor="receipient"> </label>
-        <input
-          type="text"
-          name="to"
-          id="Receipient"
-          placeholder="Receipient"
-          required pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
-          value={mailToAdd.to}
-          onChange={handleChange}
-        />
-        <label htmlFor="subject"></label>
-        <input
-          type="text"
-          name="subject"
-          id="subject"
-          placeholder="Subject"
-          value={mailToAdd.subject}
-          onChange={handleChange}
-        />
-        <label htmlFor="mail-body"></label>
-        <input
-          type="text-area"
-          name="body"
-          id="mail-body"
-          placeholder="Body"
-          value={mailToAdd.body}
-          onChange={handleChange}
-        />
+        <div className="mail-add-receipient">
+          <label htmlFor="receipient"> </label>
+          <input
+            type="text"
+            name="to"
+            id="Receipient"
+            placeholder="Receipient"
+            required
+            pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+            value={mailToAdd.to}
+            onChange={handleChange}
+          />
+          <div className="mail-add-subject"></div>
+          <label htmlFor="subject"></label>
+          <input
+            type="text"
+            name="subject"
+            id="subject"
+            placeholder="Subject"
+            value={mailToAdd.subject}
+            onChange={handleChange}
+          />
+          <div className="mail-add-body"></div>
+
+          <label htmlFor="mail-body"></label>
+          <input
+            type="text-area"
+            name="body"
+            id="mail-body"
+            placeholder="Body"
+            value={mailToAdd.body}
+            onChange={handleChange}
+          />
+        </div>
 
         <div>
-          <button>click me</button>
+          <button className="mail-add-submit-btn">click me</button>
         </div>
       </form>
     </section>
--- views/about.jsx
+++ views/about.jsx
@@ -1,7 +1,21 @@
 
 export function About() {
     return <section className="about">
-        <h2>This is a project by Alon Mlievski & Noah Markovich</h2>
-        <h1>About Page</h1>
+<div className="alon">
+    <img src="" alt="" />
+    <h1>Alon Mlievski</h1>
+    <h2>Full Stack Developer</h2>
+    <h4>Coding Academy student</h4>
+    <h4>Based in Tel Aviv</h4>
+</div>
+<div className="noah">
+    <img src="" alt="" />
+    <h1>Noah Markovich</h1>
+    <h2>Full Stack Developer</h2>
+    <h4>Coding Academy student</h4>
+    <h4>Based in Tel Aviv</h4>
+</div>
     </section>
 }
+
+
